0393bee9ef419870b30b5a9cf4e8e18740b5354f
murdoch (Duncan Murdoch) <murdoch.duncan@gmail.com>
2008-08-31 21:17:34 +0000

    Remove un-needed branch and tags
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@703 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- branches/murdoch-fonts/R/callbacks.R
+++
@@ -1,4 +0,0 @@
-rgl.setMouseCallbacks <- function(button, begin=NULL, update=NULL, end=NULL) {
-    invisible(.Call(rgl_setMouseCallbacks, as.integer(button), begin, update, end))
-}
-
--- branches/murdoch-fonts/src/R.h
+++
@@ -1,9 +0,0 @@
-#ifndef RGL_R_H
-#define RGL_R_H
-
-// This define avoids a warning about ERROR redefinition in Windows
-#define STRICT_R_HEADERS
-
-#include <R.h>
-
-#endif /* RGL_R_H */
