ebedd470e6155ad87a08b85544ee49d83f8cc4b6
dadler (Daniel Adler) <dadler@dyncall.org>
2011-05-10 23:52:09 +0000

    added mouse event handling
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@818 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- branches/cocoa/rgl/src/RGLCocoaView.h
+++ branches/cocoa/rgl/src/RGLCocoaView.h
@@ -10,16 +10,38 @@ namespace gui {
 class CocoaWindowImpl;
 }
 
-@interface RGLCocoaView : NSOpenGLView // <NSWindowDelegate>
+// TODO: protected with, #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
+@interface RGLCocoaView : NSOpenGLView <NSWindowDelegate>
 {
   gui::CocoaWindowImpl* mWindowImpl;
+  int mButtonDown; /* button id of down-event */
 }
 
 + (NSOpenGLPixelFormat*) basicPixelFormat;
 - (id)   initWithFrame: (NSRect) frameRect andWindowImpl: (gui::CocoaWindowImpl*) impl;
 - (void) drawRect:      (NSRect) rect;
+/* OpenGL event */
 - (void) reshape;
+/* UI event */
 
+- (void) mouseDown:  (NSEvent *) theEvent;
+- (void) mouseUp:    (NSEvent *) theEvent;
+- (void) mouseDragged: (NSEvent *) theEvent;
+- (void) rightMouseDown: (NSEvent *) theEvent;
+- (void) rightMouseDragged: (NSEvent *) theEvent;
+- (void) rightMouseUp: (NSEvent *) theEvent;
+- (void) otherMouseDown: (NSEvent *) theEvent;
+- (void) otherMouseDragged: (NSEvent *) theEvent;
+- (void) otherMouseUp: (NSEvent *) theEvent;
+
+- (void) scrollWheel: (NSEvent *) theEvent;
+
+- (void) rotateWithEvent: (NSEvent *) theEvent;
+- (void) magnifyWithEvent: (NSEvent *) theEvent;
+- (void) swipeWithEvent: (NSEvent *) theEvent;
+
+// - (void) mouseMoved: (NSEvent *) theEvent;
 @end
 
 #endif
+
