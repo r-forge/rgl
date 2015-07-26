a201c1b9817b1172ebe59df1068f2cafbb5f7e35
murdoch <murdoch@79a1b648-8b68-254f-b20b-c0b50f5137bf>
2015-07-26 21:00:27 +0000

    Progress...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@1264 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- pkg/rglwidget/inst/htmlwidgets/lib/rglClass/rglClass.src.js
+++ pkg/rglwidget/inst/htmlwidgets/lib/rglClass/rglClass.src.js
@@ -123,4 +123,99 @@ rglClass = function() {
                concat(this.transparent[subscene]).
                concat(this.opaque[subscene]);
     };
+
+    this.getPowerOfTwo = function(value) {
+	     var pow = 1;
+	     while(pow<value) {
+	       pow *= 2;
+	     }
+	     return pow;
+	   };
+
+	   this.handleLoadedTexture = function(texture, textureCanvas) {
+	     var gl = this.gl;
+	     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+	     gl.bindTexture(gl.TEXTURE_2D, texture);
+	     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureCanvas);
+	     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+	     gl.generateMipmap(gl.TEXTURE_2D);
+
+	     gl.bindTexture(gl.TEXTURE_2D, null);
+	   };
+
+	   this.loadImageToTexture = function(filename, texture) {
+	     var ctx = this.canvas.getContext("2d"),
+	         image = new Image()
+	         self = this;
+
+	     image.onload = function() {
+	       var w = image.width,
+	           h = image.height,
+	           canvasX = getPowerOfTwo(w),
+	           canvasY = getPowerOfTwo(h);
+	       canvas.width = canvasX;
+	       canvas.height = canvasY;
+	       ctx.imageSmoothingEnabled = true;
+	       ctx.drawImage(image, 0, 0, canvasX, canvasY);
+	       self.handleLoadedTexture(texture, canvas);
+	       self.drawScene();
+	     };
+	     image.src = filename;
+	   };
+
+	   this.drawTextToCanvas = function(text, cex, fontFamily) {
+	     var canvasX, canvasY,
+	         textX, textY,
+
+	         textHeight = 20 * cex,
+	         textColour = "white",
+
+	         backgroundColour = "rgba(0,0,0,0)",
+
+	         ctx = this.canvas.getContext("2d"),
+           i;
+
+	     ctx.font = textHeight+"px "+fontFamily;
+
+	     canvasX = 1;
+	     var widths = [];
+	     for (i = 0; i < text.length; i++)  {
+	       widths[i] = ctx.measureText(text[i]).width;
+	       canvasX = (widths[i] > canvasX) ? widths[i] : canvasX;
+	     }
+	     canvasX = this.getPowerOfTwo(canvasX);
+
+	     var offset = 2*textHeight, // offset to first baseline
+	         skip = 2*textHeight;   // skip between baselines
+	     canvasY = this.getPowerOfTwo(offset + text.length*skip);
+
+	     this.canvas.width = canvasX;
+	     this.canvas.height = canvasY;
+
+	     ctx.fillStyle = backgroundColour;
+	     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	     ctx.fillStyle = textColour;
+	     ctx.textAlign = "left";
+
+	     ctx.textBaseline = "alphabetic";
+	     ctx.font = textHeight+"px "+fontFamily;
+
+	     for(i = 0; i < text.length; i++) {
+	       textY = i*skip + offset;
+	       ctx.fillText(text[i], 0,  textY);
+	     }
+	     return {canvasX:canvasX, canvasY:canvasY,
+	             widths:widths, textHeight:textHeight,
+	             offset:offset, skip:skip};
+	   };
+
+	   this.setViewport = function(id) {
+	     this.vp = this.viewport[id];
+	     this.gl.viewport(this.vp[0], this.vp[1], this.vp[2], this.vp[3]);
+	     this.gl.scissor(this.vp[0], this.vp[1], this.vp[2], this.vp[3]);
+	   };
+
 }).call(rglClass.prototype);
