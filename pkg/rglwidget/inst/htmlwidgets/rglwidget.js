8fae964a68fc3b4529f657b5485556825073a6c4
murdoch <murdoch@79a1b648-8b68-254f-b20b-c0b50f5137bf>
2015-08-14 00:10:40 +0000

    Progress
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@1277 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- pkg/rglwidget/inst/htmlwidgets/rglwidget.js
+++ pkg/rglwidget/inst/htmlwidgets/rglwidget.js
@@ -15,6 +15,11 @@ HTMLWidgets.widget({
   renderValue: function(el, x, instance) {
 
     var rgl = new rglClass();
+//    var ctx = el.getContext("2d");
+
+//    el.innerText = x.objects["7"].type + "\n" + Object.getOwnPropertyNames(x.objects["7"]);
+//    el.innerText = x.objects["7"].type + "\n" + x.objects["7"].vertices;
+    //  ctx.fillText(c, 10,10);
     rgl.initialize(el, x);
 
   },
