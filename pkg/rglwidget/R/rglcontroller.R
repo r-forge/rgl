da42f5342af5876d18f70b98b6a23a7d92d89589
murdoch <murdoch@79a1b648-8b68-254f-b20b-c0b50f5137bf>
2015-09-05 17:45:43 +0000

    Add ageControl
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@1321 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- pkg/rglwidget/R/rglcontroller.R
+++ pkg/rglwidget/R/rglcontroller.R
@@ -52,3 +52,59 @@ propertyControl <- function(value, entries, properties, objids, values = NULL,
        param = param,
        interp = interp)
 }
+
+ageControl <- function(value, births, ages, colors = NULL, alpha = NULL,
+                       radii = NULL, vertices = NULL, normals = NULL,
+                       origins = NULL, texcoords = NULL, objids) {
+
+  lengths <- c(colors = NROW(colors), alpha = length(alpha),
+               radii = length(radii), vertices = NROW(vertices),
+               normals = NROW(normals), origins = NROW(origins),
+               texcoords = NROW(texcoords))
+  lengths <- lengths[lengths > 0]
+  n <- unique(lengths)
+  stopifnot(length(n) == 1, n == length(ages), all(diff(ages) >= 0))
+
+  ages <- c(-Inf, ages, Inf)
+  rows <- c(1, 1:n, n)
+
+  result <- list(type = "ageSetter",
+                 objids = as.integer(objids),
+                 value = value,
+                 births = births,
+                 ages = ages)
+
+  if (!is.null(colors)) {
+    colors <- col2rgb(colors)/255
+    colors <- as.numeric(colors[,rows])
+    result <- c(result, list(colors = colors))
+  }
+
+  if (!is.null(alpha))
+    result <- c(result, list(alpha = alpha[rows]))
+
+  if (!is.null(radii))
+    result <- c(result, list(radii = radii[rows]))
+
+  if (!is.null(vertices)) {
+    stopifnot(ncol(vertices) == 3)
+    result <- c(result, list(vertices = as.numeric(t(vertices[rows,]))))
+  }
+
+  if (!is.null(normals)) {
+    stopifnot(ncol(normals) == 3)
+    result <- c(result, list(normals = as.numeric(t(normals[rows,]))))
+  }
+
+  if (!is.null(origins)) {
+    stopifnot(ncol(origins) == 2)
+    result <- c(result, list(origins = as.numeric(t(origins[rows,]))))
+  }
+
+  if (!is.null(texcoords)) {
+    stopifnot(ncol(texcoords) == 2)
+    result <- c(result, list(texcoords = as.numeric(t(texcoords[rows,]))))
+  }
+
+  result
+}
