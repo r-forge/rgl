e54c212add41fa63ff02d4e4878afe45ef6afb1a
murdoch <murdoch@79a1b648-8b68-254f-b20b-c0b50f5137bf>
2015-11-01 14:18:33 +0000

    Minor fixes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@1392 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- pkg/rglwidget/R/Sweave_knitr.R
+++ pkg/rglwidget/R/Sweave_knitr.R
@@ -1,88 +1,10 @@
-##
-## Sweave device
-##
-##
-
-rgl.Sweave <- function(name, width, height, options, ...) {
-
-  # The 2.13.0 release called dev.off(), not rgl.Sweave.off()
-
-  if (getRversion() == "2.13.0" && version$status == "") {
-
-    postscript(tempfile()) # make dev.off() happy
-    if (length(getHook("on.rgl.close"))) rgl.Sweave.off() # to close the previous chunk
-  }
-
-  if (length(hook <- getHook("on.rgl.close"))) {
-    if (is.list(hook)) hook <- hook[[1]]  # test is for compatibility with R < 3.0.0
-    dev <- environment(hook)$dev
-    rgl.set(dev)
-  } else {
-    wr <- c(0, 0, width*options$resolution, height*options$resolution)
-    open3d(windowRect=wr)
-    if (is.null(delay <- options$delay)) delay <- 0.1
-    Sys.sleep(as.numeric(delay))
-    wrnew <- par3d("windowRect")
-    if (wr[3] - wr[1] != wrnew[3] - wrnew[1] ||
-        wr[4] - wr[2] != wrnew[4] - wrnew[2])
-      stop("rgl window creation error; try reducing resolution, width or height")
-    dev <- rgl.cur()
-  }
-
-  snapshotDone <- FALSE
-
-  stayOpen <- isTRUE(options$stayopen)
-
-  type <- options$outputtype
-  if (is.null(type)) type <- "png"
-
-  setHook("on.rgl.close", action="replace", function(remove=TRUE) {
-    prev.dev <- rgl.cur()
-    on.exit(rgl.set(prev.dev))
-
-    if (!snapshotDone) {
-      rgl.set(dev)
-      switch(type,
-        png = rgl.snapshot(filename=paste(name, "png", sep=".")),
-        pdf = rgl.postscript(filename=paste(name, "pdf", sep="."), fmt="pdf"),
-        eps = rgl.postscript(filename=paste(name, "eps", sep="."), fmt="eps"),
-        stop(gettextf("Unrecognized rgl outputtype: '%s'", type), domain = NA)
-      )
-      snapshotDone <<- TRUE
-    }
-
-    if (remove)
-      setHook("on.rgl.close", action="replace", NULL)
-  })
-}
-
-rgl.Sweave.off <- function() {
-  if (length(hook <- getHook("on.rgl.close"))) {
-    if (is.list(hook)) hook <- hook[[1]] # test is for R pre-3.0.0 compatibility
-    stayOpen <- environment(hook)$stayOpen
-    if (stayOpen) hook(FALSE)
-    else rgl.close()
-  }
-}
-
-##
-## Sweave snapshot
-##
-##
-
-Sweave.snapshot <- function() {
-  if (length(hook <- getHook("on.rgl.close"))) {
-    if (is.list(hook)) hook <- hook[[1]] # test is for R pre-3.0.0 compatibility
-    hook(remove = FALSE)
-  }
-}
 
 ##
 ## knitr hook functions
 ##
 ##
 
-hook_webgl <- local({
+.hook_webgl <- local({
   commonParts <- TRUE
   reuse <- TRUE
   function (before, options, envir)
@@ -131,7 +53,7 @@ hook_webgl <- local({
   }
 })
 
-hook_rgl <- function(before, options, envir) {
+.hook_rgl <- function(before, options, envir) {
   if (before) {
     newwindow <- options$rgl.newwindow
     if (!is.null(newwindow) && newwindow)
@@ -141,9 +63,6 @@ hook_rgl <- function(before, options, envir) {
     return()
   } else if (rgl.cur() == 0)
     return()
-
-  if (!requireNamespace("knitr"))
-    stop("'hook_rgl' requires the 'knitr' package.")
 
   name <- fig_path('', options)
   margin <- options$rgl.margin
@@ -178,10 +97,10 @@ save_rgl <- function(name, devices) {
 
 .setupKnitr <- function() {
   if (requireNamespace("knitr")) {
-    knit_hooks$set(webgl = hook_webgl)
-    knit_hooks$set(webGL = hook_webgl)
-    knit_hooks$set(rgl = hook_rgl)
-    environment(hook_webgl)$commonParts <- TRUE
-    environment(hook_webgl)$reuse <- TRUE
+    knit_hooks$set(webgl = .hook_webgl)
+    knit_hooks$set(webGL = .hook_webgl)
+    knit_hooks$set(rgl = .hook_rgl)
+    environment(.hook_webgl)$commonParts <- TRUE
+    environment(.hook_webgl)$reuse <- TRUE
   }
 }
