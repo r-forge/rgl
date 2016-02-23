r-forge/rgl

commit 1b7417b88840f71f56bd7cea26be0148f690bf65

Handle zero-vertex case in Javascript.
git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@1446 79a1b648-8b68-254f-b20b-c0b50f5137bf

pkg/rglwidget/DESCRIPTION
@@ -1,7 +1,7 @@
 Package: rglwidget
 Type: Package
 Title: 'rgl' in 'htmlwidgets' Framework
-Version: 0.1.1444
+Version: 0.1.1446
 Author: Duncan Murdoch
 Maintainer: Duncan Murdoch <murdoch.duncan@gmail.com>
 Description: Provides an 'htmlwidgets' framework for the 'rgl' package.

pkg/rglwidget/inst/htmlwidgets/lib/rglClass/rglClass.src.js
@@ -854,6 +854,13 @@ rglwidgetClass = function() {
       return;
     }
 
+    if (typeof obj.vertices === "undefined")
+      obj.vertices = [];
+
+    v = obj.vertices;
+    obj.vertexCount = v.length;
+    if (!obj.vertexCount) return;
+
 		if (!sprites_3d) {
 			obj.prog = gl.createProgram();
 			gl.attachShader(obj.prog, this.getShader( gl.VERTEX_SHADER,
@@ -918,10 +925,6 @@ rglwidgetClass = function() {
 		if (type === "text") {
 		  this.handleLoadedTexture(obj.texture, this.textureCanvas);
 		}
-
-    v = obj.vertices;
-    obj.vertexCount = v.length;
-    if (!obj.vertexCount) return;
 
     var stride = 3, nc, cofs, nofs, radofs, oofs, tofs, vnew, v1;
 
@@ -1186,7 +1189,7 @@ rglwidgetClass = function() {
       if (!obj.initialized)
         this.initObj(id);
 
-      if (type === "light" || type === "bboxdeco")
+      if (type === "light" || type === "bboxdeco" || !obj.vertexCount)
         return;
 
 		  if (type === "clipplanes") {

commit 388c16ec4258bf8052bf08a4d4d4821121f11b0d

Fix clip plane bug introduced in r1446
git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl@1455 79a1b648-8b68-254f-b20b-c0b50f5137bf

pkg/rglwidget/DESCRIPTION
@@ -1,7 +1,7 @@
 Package: rglwidget
 Type: Package
 Title: 'rgl' in 'htmlwidgets' Framework
-Version: 0.1.1454
+Version: 0.1.1455
 Author: Duncan Murdoch
 Maintainer: Duncan Murdoch <murdoch.duncan@gmail.com>
 Description: Provides an 'htmlwidgets' framework for the 'rgl' package.

pkg/rglwidget/inst/htmlwidgets/lib/rglClass/rglClass.src.js
@@ -1189,9 +1189,6 @@ rglwidgetClass = function() {
       if (!obj.initialized)
         this.initObj(id);
 
-      if (type === "light" || type === "bboxdeco" || !obj.vertexCount)
-        return;
-
 		  if (type === "clipplanes") {
 			  count = obj.offsets.length;
 			  var IMVClip = [];
@@ -1201,6 +1198,9 @@ rglwidgetClass = function() {
  			  obj.IMVClip = IMVClip;
         return;
 			}
+
+      if (type === "light" || type === "bboxdeco" || !obj.vertexCount)
+        return;
 
       this.setDepthTest(id);
 